5e4fc17223f6f4256aa6e21a30df4f83f0111159
EC2 Default User <ec2-user@ip-172-31-42-90.ap-northeast-1.compute.internal>
2023-02-13 18:07:30 +0000

    create list_item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
     has_many_attached :image
     
     has_many :item_recipe_maps, dependent: :destroy
+    has_many :lists, through: :list_items
     belongs_to :category
-    belongs_to :list_item
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,4 @@
 class List < ApplicationRecord
   belongs_to :customer
-  has_many :list_items
+  has_many :lists, through: :list_items
 end
