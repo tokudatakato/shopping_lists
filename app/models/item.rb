82b387781a6a13f3b794a836eab11302cded5f9d
EC2 Default User <ec2-user@ip-172-31-42-90.ap-northeast-1.compute.internal>
2023-02-19 17:43:59 +0000

    index show new

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,8 @@ class Item < ApplicationRecord
   has_many_attached :item_image
   
   has_many :item_recipe_maps, dependent: :destroy
-  has_many :lists, through: :list_items
   has_many :list_items
+  has_many :lists, through: :list_items
   belongs_to :category
   
   def self.search(keyword)
