18ce0c5395224d6d848d1ed2ed4f14bdae1c1e3c
tokudatakato <takato010619@gmail.com>
2023-02-25 12:26:25 +0000

    recipe taggings削除

--- app/models/recipe_taggings.rb
+++
@@ -1,9 +0,0 @@
-class PostTagging < ApplicationRecord
-    
-  belongs_to :recipe
-  belongs_to :hashtag
-  
-  validates :recipe_id, presence: true
-  validates :hashtag_id, presence: true
-  validates :recipe_id, uniqueness: { scope: :hashtag_id }
-end
